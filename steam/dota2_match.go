1063e1081af0c64819208996b3a9743635746330
aoisensi <aoisensi@outlook.jp>
2020-01-06 01:18:24 +0900

    add backpack_3

--- steam/dota2_match.go
+++ steam/dota2_match.go
@@ -55,6 +55,7 @@ type Dota2MatchDetailsPlayer struct {
 	Backpack0         int      `json:"backpack_0"`
 	Backpack1         int      `json:"backpack_1"`
 	Backpack2         int      `json:"backpack_2"`
+	Backpack3         int      `json:"backpack_3"`
 	Kills             int      `json:"kills"`
 	Deaths            int      `json:"deaths"`
 	Assists           int      `json:"assists"`
@@ -89,6 +90,7 @@ type Dota2MatchDetailsPlayer struct {
 		Backpack0 int    `json:"backpack_0"`
 		Backpack1 int    `json:"backpack_1"`
 		Backpack2 int    `json:"backpack_2"`
+		Backpack3 int    `json:"backpack_3"`
 	} `json:"additional_units,omitempty"`
 }
 
