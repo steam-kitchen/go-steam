fdd5a99dbcafda322754b387d149391a1ce0651f
aoisensi <me@aoisensi.info>
2020-10-06 23:56:36 +0900

    remove ids

--- steam/id.go
+++ steam/id.go
@@ -7,28 +7,18 @@ const idMagicNumber int64 = 76561197960265728
 const idAnonymouse = SteamID3(4294967295)
 
 type SteamID interface {
-	SteamID() int64
-	FriendID() int32
 	SteamID64() SteamID64
 	SteamID3() SteamID3
 }
 
 type SteamID64 int64
 
-func (s SteamID64) SteamID() int64 {
-	return int64(s)
-}
-
-func (s SteamID64) FriendID() int32 {
-	return int32(int64(s) - idMagicNumber)
-}
-
 func (s SteamID64) SteamID64() SteamID64 {
 	return s
 }
 
 func (s SteamID64) SteamID3() SteamID3 {
-	return SteamID3(s.FriendID())
+	return SteamID3(int64(s) - idMagicNumber)
 }
 
 func (s SteamID64) String() string {
@@ -37,16 +27,8 @@ func (s SteamID64) String() string {
 
 type SteamID3 uint32
 
-func (s SteamID3) SteamID() int64 {
-	return int64(s) + idMagicNumber
-}
-
-func (s SteamID3) FriendID() int32 {
-	return int32(s)
-}
-
 func (s SteamID3) SteamID64() SteamID64 {
-	return SteamID64(s.SteamID())
+	return SteamID64(int64(s) + idMagicNumber)
 }
 
 func (s SteamID3) SteamID3() SteamID3 {
